98c8b22cf7be7f4fd555db5cca319ce4ed52e43b
Zirafinu <focke.sebastian@gmail.com>
2025-04-03 21:19:28 +0200

    fixed CRC implementation

--- source/crc/sw_crc.cpp
+++ source/crc/sw_crc.cpp
@@ -1,11 +1,5 @@
 #include <cstdint>
 
-const uint32_t crc32_tab[16] = {
-    0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, //
-    0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3, //
-    0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, //
-    0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, //
-};
 const uint32_t crc32_bzip2_tab[16] = {
     0x00000000, 0x04C11DB7, 0x09823B6E, 0x0D4326D9, //
     0x130476DC, 0x17C56B6B, 0x1A864DB2, 0x1E475005, //
@@ -23,8 +17,8 @@ uint32_t crc(const void *mem_begin, const void *mem_end) noexcept
   while (p != mem_end)
   {
     const uint8_t byte = *p++;
-    crc = crc32_tab[(crc ^ (byte >> 4)) & 0xF] ^ (crc >> 4);
-    crc = crc32_tab[(crc ^ byte) & 0xF] ^ (crc >> 4);
+    crc = (crc << 4) ^ crc32_bzip2_tab[((crc >> 28) ^ (byte >> 4)) & 0xF];
+    crc = (crc << 4) ^ crc32_bzip2_tab[((crc >> 28) ^ byte) & 0xF];
   }
   return crc ^ ~0U;
 }
--- source/crc/test_sw_crc.cpp
+++ source/crc/test_sw_crc.cpp
@@ -3,11 +3,9 @@
 
 int main(int argc, char **args)
 {
-    char message[] = "123456789";
-    const auto crc_value = crc(message, &message[std::size(message)]);
+    const char message[] = "123456789";
+    const auto crc_value = crc(message, &message[std::size(message) - 1]);
 
-    constexpr uint32_t crc_value_crc32 = 0xCBF43926;
-    return crc_value == crc_value_crc32;
-
-    // constexpr uint32_t crc_value_bzip2 = 0xFC891918;
+    constexpr uint32_t crc_value_bzip2 = 0xFC891918;
+    return crc_value != crc_value_bzip2;
 }
