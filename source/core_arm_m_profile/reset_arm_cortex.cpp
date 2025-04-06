b314c5664953bba2c5b099dfc5474c456132c3b9
Zirafinu <focke.sebastian@gmail.com>
2025-04-06 17:48:38 +0200

    the isr_vector is now actually part of the binary the reset handler falls throught to the ini_mem function

--- source/core_arm_m_profile/reset_arm_cortex.cpp
+++ source/core_arm_m_profile/reset_arm_cortex.cpp
@@ -101,28 +101,18 @@ extern "C"
   // the initial_stack address
   extern const size_t estack;
 
-  void init_mem() noexcept
+  __attribute__((used)) void init_mem() noexcept
   {
     // set core in hsi clock mode, with pll enabled (just push out the
     // registers)
 
     // initialize the crc accelerator and set up a dma, to feed the application
     // into it
-    for (auto *src = &data_load_start, *dst = &data_start; src != &data_load_end;
-         ++src, ++dst)
-    {
-      *dst = *src;
-    }
-    for (auto *dst = &bss_start; dst != &bss_end; ++dst)
-    {
-      *dst = 0;
-    }
+
+    std::memcpy(&data_start, &data_load_start, size_t(&data_load_end) - size_t(&data_load_start));
+    std::memset(&bss_start, 0, size_t(&bss_end) - size_t(&bss_start));
+
     // __libc_init_array();
     main();
   }
-
-  __attribute__((section(".isr_vector"))) const void *const isr_vector[] = {
-      &estack,
-      reinterpret_cast<void *const>(&Reset_Handler),
-  };
 }
