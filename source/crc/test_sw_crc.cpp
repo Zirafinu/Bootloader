1bf0f7a17fcb23215a27c65f734b8737961eb858
Zirafinu <focke.sebastian@gmail.com>
2025-04-06 17:01:11 +0200

    fixed broken test

--- source/crc/test_sw_crc.cpp
+++ source/crc/test_sw_crc.cpp
@@ -1,10 +1,10 @@
-#include "../bootloader.h"
+#include <crc.h>
 #include <array>
 
 int main(int argc, char **args)
 {
     const char message[] = "123456789";
-    const auto crc_value = crc(message, &message[std::size(message) - 1]);
+    const auto crc_value = crc::compute(message, &message[std::size(message) - 1]);
 
     constexpr uint32_t crc_value_bzip2 = 0xFC891918;
     return crc_value != crc_value_bzip2;
