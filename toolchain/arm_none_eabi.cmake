0b6a9da74a67e14706d69e0abe9801e4dd8403b4
sebastian rausch <focke.sebastian@gmail.com>
2025-04-07 21:00:27 +0200

    selected coretex-m4 as arm target

--- toolchain/arm_none_eabi.cmake
+++ toolchain/arm_none_eabi.cmake
@@ -41,6 +41,7 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 # Object build options
 # -O0                   No optimizations, reduce compilation time and make debugging produce the expected results.
+# -mcpu=cortex-m4       generate m4 code
 # -mthumb               Generat thumb instructions.
 # -fno-builtin          Do not use built-in functions provided by GCC.
 # -Wall                 Print only standard warnings, for all use Wextra
@@ -48,7 +49,7 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 # -fdata-sections       Place each data item into its own section in the output file.
 # -fomit-frame-pointer  Omit the frame pointer in functions that don’t need one.
 # -mabi=aapcs           Defines enums to be a variable sized type.
-set(OBJECT_GEN_FLAGS "-mthumb -fno-builtin -Wall -ffunction-sections -fdata-sections -fomit-frame-pointer -mabi=aapcs")
+set(OBJECT_GEN_FLAGS "-mcpu=cortex-m4 -mthumb -fno-builtin -Wall -ffunction-sections -fdata-sections -fomit-frame-pointer -mabi=aapcs")
 
 set(CMAKE_C_FLAGS_INIT   "${OBJECT_GEN_FLAGS} " CACHE INTERNAL "C Compiler options")
 set(CMAKE_CXX_FLAGS_INIT "${OBJECT_GEN_FLAGS} -fno-exceptions " CACHE INTERNAL "C++ Compiler options")
