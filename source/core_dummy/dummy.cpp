f1ec5b3d73f70cfa38873c1e542d99aaa4dab237
sebastian rausch <focke.sebastian@gmail.com>
2025-04-11 21:57:54 +0200

    restructuring project

--- source/core_dummy/dummy.cpp
+++ source/core_dummy/dummy.cpp
@@ -5,13 +5,9 @@
 
 extern "C"
 {
-  uint32_t application_skip_request;
-  constexpr uint32_t application_skip_request_true = 0xC1AEE345UL;
-
+  uint32_t application_skip_request = 0;
   bool application_is_valid() noexcept { return true; }
   bool application_backup_is_valid() noexcept { return true; }
-  bool application_requested_skipping() noexcept { return application_skip_request == application_skip_request_true; }
-  void reset_application_skipping_request() noexcept { application_skip_request = ~application_skip_request_true; }
 
   void copy_backup_to_application() noexcept {}
   void jump_to_application() noexcept { std::exit(1); }
