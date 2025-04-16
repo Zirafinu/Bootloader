04f8f2b732df4c9fc1a0d110f1974cb0dfed3d46
Sebastian Rausch <rausch@mechatronic.de>
2025-04-16 14:01:12 +0200

    adding cube project dependencies

--- examples/stm32f429/sources/reset_arm_cortex.cpp
+++ examples/stm32f429/sources/reset_arm_cortex.cpp
@@ -8,6 +8,10 @@
 #include <flash_layout.h>
 #include <gzip.h>
 
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_pwr.h"
+#include "stm32f4xx_ll_system.h"
+
 extern "C" {
 
 // the applications reset handler address
@@ -29,10 +33,40 @@ extern size_t bss_end;
 __attribute__((used)) void init_mem() noexcept {
     // set core in hsi clock mode, with pll enabled (just push out the
     // registers)
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+    while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_1) {
+    }
+    LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE3);
+    LL_PWR_DisableOverDriveMode();
+    LL_RCC_HSI_Enable();
 
+    /* Wait till HSI is ready */
+    while (LL_RCC_HSI_IsReady() != 1) {
+    }
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI) {
+    }
+
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_8, 50, LL_RCC_PLLP_DIV_2);
+    LL_RCC_PLL_Enable();
+
+    /* Wait till PLL is ready */
+    while (LL_RCC_PLL_IsReady() != 1) {
+    }
+    while (LL_PWR_IsActiveFlag_VOS() == 0) {
+    }
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_16);
+    LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_16);
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+    /* Wait till System clock is ready */
+    while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) {
+    }
     // initialize the crc accelerator and set up a dma, to feed the application
     // into it
 
+    // initialize loaded memory sections
     std::memcpy(&data_start, &data_load_start, size_t(&data_load_end) - size_t(&data_load_start));
     std::memset(&bss_start, 0, size_t(&bss_end) - size_t(&bss_start));
 
