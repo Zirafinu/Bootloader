8edf39824565f78ddd8554feae25d52c05f64506
Zirafinu <focke.sebastian@gmail.com>
2025-09-13 19:03:44 +0200

    added a blinky executable

---
+++ examples/stm32f407/stm32f407/blinky.cpp
@@ -0,0 +1,25 @@
+#include <cstdint>
+#include <stm32f407xx.h>
+
+int main() {
+    // enable GPIO bank D
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+    // the LEDs are connected to pins D12-D15
+    GPIOD->MODER = GPIO_MODER_MODE12_0 | GPIO_MODER_MODE13_0 | GPIO_MODER_MODE14_0 | GPIO_MODER_MODE15_0;
+
+    std::size_t leds = 0;
+
+    for (;;) {
+        ++leds;
+        uint32_t const on_set = leds & 0xF;
+        uint32_t const off_set = ((~leds) & 0xF) << 16;
+        GPIOD->BSRR = (on_set | off_set) << 12;
+
+        // might be enough delay to see them blink
+        for (int i = 0; i < 1000; ++i) {
+            __NOP();
+        }
+    }
+
+    return 0;
+}
