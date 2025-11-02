330ae7cee86bbdd96a1ab541c0f398bf4c05b3e7
Zirafinu <focke.sebastian@gmail.com>
2025-11-02 09:17:56 +0100

    skip request resets now write 0
    before the magic numbers could collide.

--- source/skip_application/skip_application_noinit_ram.cpp
+++ source/skip_application/skip_application_noinit_ram.cpp
@@ -3,15 +3,17 @@
 namespace {
 constexpr uint32_t application_skip_request_true = 0xC1AE'E345UL;
 constexpr uint32_t application_update_request_true = 0xC1AE'543EUL;
+constexpr uint32_t no_request = 0;
 } // namespace
 
 extern "C" uint32_t application_skip_request;
+
 namespace skip_application {
 bool skip_is_requested() noexcept { return application_skip_request == application_skip_request_true; }
-void skip_reset_request() noexcept { application_skip_request = ~application_skip_request_true; }
+void skip_reset_request() noexcept { application_skip_request = no_request; }
 void skip_request() noexcept { application_skip_request = application_skip_request_true; }
 
 bool update_is_requested() noexcept { return application_skip_request == application_update_request_true; }
-void update_reset_request() noexcept { application_skip_request = ~application_update_request_true; }
+void update_reset_request() noexcept { application_skip_request = no_request; }
 void update_request() noexcept { application_skip_request = application_update_request_true; }
 } // namespace skip_application
